472ebbc0f8461d2e627629bd52e0993d6fc8240f
air_kinu <techgit1024@gmail.com>
2025-12-06 22:39:02 +0900

    rename: OutsideSceneがHouseSceneになっていたのを修正

--- src/app/game/components/scenes/shared/OutsideScene.tsx
+++ src/app/game/components/scenes/shared/OutsideScene.tsx
@@ -3,7 +3,7 @@ import TouchButton from "../TouchButton";
 export default function OutsideScene() {
   return (
     <>
-      <Situation bg="/images/shared/HouseScene.jpg">
+      <Situation bg="/images/shared/OutsideScene.jpg">
         <TouchButton top="48%" left="25%" />
         <TouchButton top="68%" left="60%" />
         <TouchButton top="35%" left="60%" />
