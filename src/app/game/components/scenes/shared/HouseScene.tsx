de62291d5c6d939d50c8d869a4050c3e3a440963
air_kinu <techgit1024@gmail.com>
2025-12-06 22:10:55 +0900

    feat: シチュエーションとタッチボタンの相対位置を保って表示するコンポーネント作成

--- src/app/game/components/scenes/shared/HouseScene.tsx
+++ src/app/game/components/scenes/shared/HouseScene.tsx
@@ -1,7 +1,11 @@
+import Situation from "../Situation";
+import TouchButton from "../TouchButton";
 export default function HouseScene() {
   return (
     <>
-      <div>HouseScene</div>
+      <Situation bg="/images/shared/HouseScene.jpg">
+        <TouchButton top="48%" left="25%" />
+      </Situation>
     </>
   );
 }
