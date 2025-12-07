ffc008953de6ed463549536c3986a1ace7a7886e
air_kinu <techgit1024@gmail.com>
2025-12-07 10:43:50 +0900

    feat: /から/titleへのリダイレクトを実装

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
+import { redirect } from "next/navigation";
 import HarvestStatus from "./game/components/HarvestStatus";
 import Milk_MarketScene from "./game/components/scenes/milk/Milk_MarketScene";
 import InsideHouseScene from "./game/components/scenes/shared/InsideHouseScene";
 import OutsideScene from "./game/components/scenes/shared/OutsideScene";
 
 export default function Home() {
+  redirect("/title");
   return (
     <>
-      <OutsideScene />
+      {/* <OutsideScene />
       <InsideHouseScene />
-      <Milk_MarketScene />
+      <Milk_MarketScene /> */}
     </>
   );
 }
